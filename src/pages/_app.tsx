78724fcdc737e611b1d866059b041cbe1cd1ee9b
Pierre Le Guen <26087574+PierreLeGuen@users.noreply.github.com>
2023-04-19 12:03:32 +0200

    wip - sidebar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
-import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import { api } from "~/utils/api";
+config.autoAddCss = false;
 
 import "~/styles/globals.css";
 
