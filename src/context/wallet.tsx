da97c73d035e7df65f606986d264acd52c998771
Pierre Le Guen <26087574+PierreLeGuen@users.noreply.github.com>
2023-05-02 15:55:03 +0200

    Disable some wallets

--- src/context/wallet.tsx
+++ src/context/wallet.tsx
@@ -1,21 +1,9 @@
-import { setupCoin98Wallet } from "@near-wallet-selector/coin98-wallet";
 import type { AccountState, WalletSelector } from "@near-wallet-selector/core";
 import { setupWalletSelector } from "@near-wallet-selector/core";
-import { setupHereWallet } from "@near-wallet-selector/here-wallet";
-import { setupMathWallet } from "@near-wallet-selector/math-wallet";
-import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
 import type { WalletSelectorModal } from "@near-wallet-selector/modal-ui";
 import { setupModal } from "@near-wallet-selector/modal-ui";
-import { setupNarwallets } from "@near-wallet-selector/narwallets";
-import { setupNearSnap } from "@near-wallet-selector/near-snap";
 import { setupNearWallet } from "@near-wallet-selector/near-wallet";
-import { setupNearFi } from "@near-wallet-selector/nearfi";
-import { setupNightly } from "@near-wallet-selector/nightly";
-import { setupNightlyConnect } from "@near-wallet-selector/nightly-connect";
-import { setupSender } from "@near-wallet-selector/sender";
 // import { setupWalletConnect } from "@near-wallet-selector/wallet-connect";
-import { setupWelldoneWallet } from "@near-wallet-selector/welldone-wallet";
-import { setupXDEFI } from "@near-wallet-selector/xdefi";
 import type { ReactNode } from "react";
 import React, {
   useCallback,
@@ -26,11 +14,8 @@ import React, {
 } from "react";
 import { distinctUntilChanged, map } from "rxjs";
 
-import { setupFinerWallet } from "@near-wallet-selector/finer-wallet";
 import { setupLedger } from "@near-wallet-selector/ledger";
 import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
-import { setupNeth } from "@near-wallet-selector/neth";
-import { setupOptoWallet } from "@near-wallet-selector/opto-wallet";
 import Loading from "../components/loading";
 
 declare global {
@@ -66,23 +51,23 @@ export const WalletSelectorContextProvider: React.FC<{
         setupMyNearWallet(),
         setupLedger(),
         setupNearWallet(),
-        setupSender(),
-        setupMathWallet(),
-        setupNightly(),
-        setupMeteorWallet(),
-        setupNearSnap(),
-        setupNarwallets(),
-        setupWelldoneWallet(),
-        setupHereWallet(),
-        setupCoin98Wallet(),
-        setupNearFi(),
-        setupNeth({
-          gas: "300000000000000",
-          bundle: false,
-        }),
-        setupOptoWallet(),
-        setupFinerWallet(),
-        setupXDEFI(),
+        // setupSender(),
+        // setupMathWallet(),
+        // setupNightly(),
+        // setupMeteorWallet(),
+        // setupNearSnap(),
+        // setupNarwallets(),
+        // setupWelldoneWallet(),
+        // setupHereWallet(),
+        // setupCoin98Wallet(),
+        // setupNearFi(),
+        // setupNeth({
+        //   gas: "300000000000000",
+        //   bundle: false,
+        // }),
+        // setupOptoWallet(),
+        // setupFinerWallet(),
+        // setupXDEFI(),
         // setupWalletConnect({
         //   projectId: "c4f79cc...",
         //   metadata: {
@@ -92,15 +77,15 @@ export const WalletSelectorContextProvider: React.FC<{
         //     icons: ["https://avatars.githubusercontent.com/u/37784886"],
         //   },
         // }),
-        setupNightlyConnect({
-          url: "wss://relay.nightly.app/app",
-          appMetadata: {
-            additionalInfo: "",
-            application: "NEAR Wallet Selector",
-            description: "Example dApp used by NEAR Wallet Selector",
-            icon: "https://near.org/wp-content/uploads/2020/09/cropped-favicon-192x192.png",
-          },
-        }),
+        // setupNightlyConnect({
+        //   url: "wss://relay.nightly.app/app",
+        //   appMetadata: {
+        //     additionalInfo: "",
+        //     application: "NEAR Wallet Selector",
+        //     description: "Example dApp used by NEAR Wallet Selector",
+        //     icon: "https://near.org/wp-content/uploads/2020/09/cropped-favicon-192x192.png",
+        //   },
+        // }),
       ],
     });
     const _modal = setupModal(_selector, {
