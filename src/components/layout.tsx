c92596bedf7c4ddc270c541df000e993cdcd781d
Pierre Le Guen <26087574+PierreLeGuen@users.noreply.github.com>
2023-05-04 13:20:26 +0200

    top bar: handle connected wallet

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,7 +9,7 @@ interface LayoutProps {
 const Layout: React.FC<{
   children: ReactNode;
 }> = ({ children }) => {
-  const { modal } = useWalletSelector();
+  const { modal, accounts, accountId } = useWalletSelector();
 
   const handleSignIn = () => {
     modal.show();
@@ -21,14 +21,27 @@ const Layout: React.FC<{
       <div className="flex flex-1 flex-col">
         <div className="flex flex-row px-8 py-2 shadow-md">
           <div className="flex flex-1"></div>
-          <button
-            className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-            onClick={() => {
-              handleSignIn();
-            }}
-          >
-            Ⓝ Sign In
-          </button>
+          {accounts.length > 0 && (
+            // TODO: switch account, disconnect, ...
+            <button
+              className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+              onClick={() => {
+                handleSignIn();
+              }}
+            >
+              Ⓝ {accountId}
+            </button>
+          )}
+          {accounts.length === 0 && (
+            <button
+              className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+              onClick={() => {
+                handleSignIn();
+              }}
+            >
+              Ⓝ Sign In
+            </button>
+          )}
         </div>
         <div className="h-full bg-slate-100">
           <div>{children}</div>
