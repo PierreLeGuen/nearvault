17e94f8f2c18565a0ed9cc78eef80a1543ef7e13
Pierre LE GUEN <26087574+PierreLeGuen@users.noreply.github.com>
2023-05-11 16:23:26 +0200

    Get account from local store, not walletselector.

--- src/store/slices/wallet.ts
+++ src/store/slices/wallet.ts
@@ -3,10 +3,14 @@ import { type StateCreator } from "zustand";
 
 export interface WalletState {
   publicKey: PublicKey | null;
+  accountId: string | null;
   setPublicKey: (publicKey: PublicKey) => void;
+  setAccountId: (accountId: string) => void;
 }
 
 export const createWalletSlice: StateCreator<WalletState> = (set, get) => ({
   publicKey: null,
+  accountId: null,
   setPublicKey: (publicKey) => set({ publicKey }),
+  setAccountId: (accountId) => set({ accountId }),
 });
