e3d8b064ed2dc5671ac297940f463130a67dfbed
Pierre LE GUEN <26087574+PierreLeGuen@users.noreply.github.com>
2023-05-17 14:35:17 +0200

    Rework letter profile pic

--- src/components/Sidebar/LetterProfilePicture.tsx
+++ src/components/Sidebar/LetterProfilePicture.tsx
@@ -1,13 +1,7 @@
 const LetterProfilePicture = ({ letter }: { letter: string }) => {
   return (
-    <div className="flex items-center">
-      <div className="relative">
-        <div className="notion-record-icon notranslate flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-md">
-          <div className="flex h-8 w-8 items-center justify-center rounded-md bg-gray-200 uppercase text-gray-600">
-            <div className="h-6 text-base">{letter}</div>
-          </div>
-        </div>
-      </div>
+    <div className="flex h-8 w-8 items-center justify-center rounded-md bg-gray-200 font-bold uppercase text-gray-600">
+      {letter}
     </div>
   );
 };
