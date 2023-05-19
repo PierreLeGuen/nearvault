c08a7709c9a065f7e4ab50f9a09a47c9a59bc051
Pierre LE GUEN <26087574+PierreLeGuen@users.noreply.github.com>
2023-05-19 15:43:19 +0200

    Update default prose config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: "75ch", // add required value here
+          },
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 } satisfies Config;
