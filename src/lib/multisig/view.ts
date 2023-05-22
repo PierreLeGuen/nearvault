b92c33f8be6ad7500e3488b90ca21d4bf9c479f2
Pierre LE GUEN <26087574+PierreLeGuen@users.noreply.github.com>
2023-05-22 16:58:03 +0200

    Better view of pending requests

--- src/lib/multisig/view.ts
+++ src/lib/multisig/view.ts
@@ -1,14 +1,121 @@
 import * as naj from "near-api-js";
-import { type Action } from "near-api-js/lib/transaction";
+
+export enum MultiSigRequestActionType {
+  CreateAccount = "CreateAccount",
+  DeployContract = "DeployContract",
+  AddMember = "AddMember",
+  DeleteMember = "DeleteMember",
+  AddKey = "AddKey",
+  SetNumConfirmations = "SetNumConfirmations",
+  SetActiveRequestsLimit = "SetActiveRequestsLimit",
+  Transfer = "Transfer",
+  NearEscrowTransfer = "NearEscrowTransfer",
+  FTEscrowTransfer = "FTEscrowTransfer",
+  FunctionCall = "FunctionCall",
+}
+interface CreateAccountAction {
+  type: MultiSigRequestActionType.CreateAccount;
+}
+
+interface DeployContractAction {
+  type: MultiSigRequestActionType.DeployContract;
+  code: string; // Base64VecU8
+}
+
+interface AddMemberAction {
+  type: MultiSigRequestActionType.AddMember;
+  member: MultisigMember;
+}
+
+interface DeleteMemberAction {
+  type: MultiSigRequestActionType.DeleteMember;
+  member: MultisigMember;
+}
+
+interface AddKeyAction {
+  type: MultiSigRequestActionType.AddKey;
+  public_key: string; // PublicKey
+  permission?: FunctionCallPermission;
+}
+
+interface SetNumConfirmationsAction {
+  type: MultiSigRequestActionType.SetNumConfirmations;
+  num_confirmations: number;
+}
+
+interface SetActiveRequestsLimitAction {
+  type: MultiSigRequestActionType.SetActiveRequestsLimit;
+  active_requests_limit: number;
+}
+
+interface TransferAction {
+  type: MultiSigRequestActionType.Transfer;
+  amount: string; // U128
+}
+
+interface NearEscrowTransferAction {
+  type: MultiSigRequestActionType.NearEscrowTransfer;
+  receiver_id: string; // AccountId
+  amount: string; // U128
+  label: string;
+  is_cancellable: boolean;
+}
+
+interface FTEscrowTransferAction {
+  type: MultiSigRequestActionType.FTEscrowTransfer;
+  receiver_id: string; // AccountId
+  amount: string; // U128
+  token_id: string; // AccountId
+  label: string;
+  is_cancellable: boolean;
+}
+
+interface MultisigMember {
+  AccessKey: {
+    public_key: PublicKey;
+  };
+  Account: {
+    account_id: string; // AccountId
+  };
+}
+
+type PublicKey = string; // In near-api-js, PublicKey is usually represented as a string.
+
+interface FunctionCallPermission {
+  allowance: string | null; // U128
+  receiver_id: string; // AccountId
+  method_names: string[];
+}
+
+interface FunctionCallAction {
+  type: MultiSigRequestActionType.FunctionCall; // Add FunctionCall to MultiSigRequestActionType enum
+  method_name: string;
+  args: string; // Base64VecU8
+  deposit: string; // U128
+  gas: string; // U64
+}
+
+type MultiSigAction =
+  | CreateAccountAction
+  | DeployContractAction
+  | AddMemberAction
+  | DeleteMemberAction
+  | AddKeyAction
+  | SetNumConfirmationsAction
+  | SetActiveRequestsLimitAction
+  | TransferAction
+  | NearEscrowTransferAction
+  | FTEscrowTransferAction
+  | FunctionCallAction;
+
+export interface MultisigRequest {
+  receiver_id: string;
+  actions: MultiSigAction[];
+}
 
 interface MultisigViewFunction extends naj.Contract {
   list_request_ids(): Promise<string[]>;
   get_request({ request_id }: { request_id: string }): Promise<MultisigRequest>;
-}
-
-export interface MultisigRequest {
-  receiver_id: string;
-  actions: Action[];
 }
 
 const MultisigViewContract = (
