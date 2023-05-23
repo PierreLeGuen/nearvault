6ced0c25f40f13225dcc8792abd0f6ace6da99af
Pierre LE GUEN <26087574+PierreLeGuen@users.noreply.github.com>
2023-05-23 14:05:18 +0200

    Added payments section

---
+++ src/components/Sidebar/PaymentsSection.tsx
@@ -0,0 +1,48 @@
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import { useState } from "react";
+
+const PaymentsSection = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <div className="mb-1 w-full">
+      <button
+        className="focus:outline-grey-300 mb-1 flex h-11 w-full items-center justify-between rounded-lg border-4 border-white p-2 font-sans last:mb-0 hover:bg-slate-100"
+        onClick={toggleExpanded}
+      >
+        <div>Payments</div>
+        <div>
+          <FontAwesomeIcon icon={expanded ? faChevronUp : faChevronDown} />
+        </div>
+      </button>
+      {expanded && (
+        <div className="pl-4">
+          <div>
+            <Link
+              className="cursor-pointer hover:text-blue-500"
+              href="/payments/escrow"
+            >
+              Escrow Payments
+            </Link>
+          </div>
+          <div>
+            <Link
+              className="cursor-pointer hover:text-blue-500"
+              href="/payments/recurring"
+            >
+              Recurring Payments
+            </Link>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PaymentsSection;
