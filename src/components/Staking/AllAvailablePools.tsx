b947c38ade96f0a71ec8b698ac6cc92cc5316e71
Pierre LE GUEN <26087574+PierreLeGuen@users.noreply.github.com>
2023-05-26 10:19:31 +0200

    Add stake view

---
+++ src/components/Staking/AllAvailablePools.tsx
@@ -0,0 +1,141 @@
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { initStakingContract } from "~/lib/staking/contract";
+import usePersistingStore from "~/store/useStore";
+
+type PoolId = string;
+type Percentage = number;
+
+type Pool = {
+  id: PoolId;
+  status: "active" | "inactive";
+  fees: Percentage;
+};
+
+const AllAvailablePools = (): JSX.Element => {
+  const { newNearConnection } = usePersistingStore();
+  const [pools, setPools] = useState<Map<PoolId, Pool>>(new Map());
+  const [searchInput, setSearchInput] = useState<string>("");
+
+  const { isLoading, isError } = useQuery(
+    ["allAvailablePools"],
+    async () => {
+      const res = await fetch("https://api.kitwallet.app/stakingPools");
+      const data = await (res.json() as Promise<PoolId[]>);
+      return data;
+    },
+    {
+      async onSuccess(poolsFromApi) {
+        const activePools: Map<PoolId, Pool> = new Map();
+        const inactivePools: Map<PoolId, Pool> = new Map();
+
+        const n = await newNearConnection();
+        const validatorsRes = await n.connection.provider.validators(null);
+
+        // Create a Set for constant time lookups
+        const validatorSet = new Set(
+          validatorsRes.current_validators.map(
+            (validator) => validator.account_id
+          )
+        );
+
+        // Parallelize calls to get_reward_fee_fraction
+        const poolPromises = poolsFromApi.map(async (pool) => {
+          const contract = initStakingContract(await n.account(""), pool);
+          const fees = await contract.get_reward_fee_fraction();
+
+          if (validatorSet.has(pool)) {
+            activePools.set(pool, {
+              id: pool,
+              status: "active",
+              fees: Number(
+                ((fees.numerator / fees.denominator) * 100).toFixed(2)
+              ),
+            });
+          } else {
+            inactivePools.set(pool, {
+              id: pool,
+              status: "inactive",
+              fees: Number(
+                ((fees.numerator / fees.denominator) * 100).toFixed(2)
+              ),
+            });
+          }
+        });
+
+        // Wait for all promises to resolve
+        await Promise.all(poolPromises);
+
+        const sortedPools: Map<PoolId, Pool> = new Map([
+          ...activePools,
+          ...inactivePools,
+        ]);
+
+        setPools(sortedPools);
+      },
+    }
+  );
+
+  const filteredPools = [...pools.values()].filter(
+    (
+      pool // added filtering based on input
+    ) => pool.id.toLowerCase().includes(searchInput.toLowerCase())
+  );
+
+  if (isLoading || pools.size === 0) return <div>Loading...</div>;
+  if (isError) return <div>Error occurred while fetching pools</div>;
+  return (
+    <>
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="Search pools"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          className="rounded-md border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-600"
+        />
+      </div>
+      <div className="mb-4 text-sm">
+        More information on pools check{" "}
+        <a
+          href="https://near-staking.com/"
+          target="_blank"
+          className="text-blue-600 hover:underline"
+        >
+          NEAR Staking
+        </a>
+      </div>
+      <div className="flex flex-col">
+        {filteredPools.map((pool) => (
+          <div
+            key={pool.id}
+            className="m-2 flex items-center justify-between rounded-md border bg-white p-4 shadow"
+          >
+            <div>
+              <a
+                className="mb-2 text-lg font-bold text-blue-500 hover:underline"
+                href={"https://near-staking.com/validator/" + pool.id}
+                target="_blank"
+              >
+                {pool.id}
+              </a>
+              <p>
+                Status: <span className="font-semibold">{pool.status}</span>
+              </p>
+              <p>
+                Fees: <span className="font-semibold">{pool.fees} %</span>
+              </p>
+            </div>
+            {pool.status === "active" && (
+              <button className="rounded-md bg-blue-600 px-4 py-2 text-white">
+                Stake
+              </button>
+            )}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default AllAvailablePools;
---
+++ src/components/Staking/WalletsDropDown.tsx
@@ -0,0 +1,98 @@
+import { Combobox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import { Fragment, useState } from "react";
+import { type WalletPretty } from "~/pages/staking/stake";
+
+const WalletsDropDown = ({
+  wallets,
+  selectedWallet,
+  setSelectedWallet,
+}: {
+  wallets: WalletPretty[];
+  selectedWallet: WalletPretty | undefined;
+  setSelectedWallet: (wallet: WalletPretty) => void;
+}) => {
+  const [query, setQuery] = useState("");
+
+  const filteredPeople =
+    query === ""
+      ? wallets
+      : wallets.filter((walletPretty) =>
+          walletPretty.prettyName
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
+
+  return (
+    <Combobox value={selectedWallet} onChange={setSelectedWallet}>
+      <div className="relative mt-1">
+        <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+          <Combobox.Input
+            className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+            displayValue={(walletPretty: WalletPretty) =>
+              walletPretty.prettyName
+            }
+            onChange={(event) => setQuery(event.target.value)}
+          />
+          <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+            <ChevronUpDownIcon
+              className="h-5 w-5 text-gray-400"
+              aria-hidden="true"
+            />
+          </Combobox.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          leave="transition ease-in duration-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          afterLeave={() => setQuery("")}
+        >
+          <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            {filteredPeople.length === 0 && query !== "" ? (
+              <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
+                Nothing found.
+              </div>
+            ) : (
+              filteredPeople.map((walletPretty) => (
+                <Combobox.Option
+                  key={walletPretty.walletDetails.id}
+                  className={({ active }) =>
+                    `relative cursor-default select-none list-none py-2 pl-10 pr-4 ${
+                      active ? "bg-teal-600 text-white" : "text-gray-900"
+                    }`
+                  }
+                  value={walletPretty}
+                >
+                  {({ selected, active }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {walletPretty.prettyName}
+                      </span>
+                      {selected ? (
+                        <span
+                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                            active ? "text-white" : "text-teal-600"
+                          }`}
+                        >
+                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Combobox.Option>
+              ))
+            )}
+          </Combobox.Options>
+        </Transition>
+      </div>
+    </Combobox>
+  );
+};
+
+export default WalletsDropDown;
