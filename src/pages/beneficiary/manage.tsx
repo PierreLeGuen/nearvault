de2e888eb511db8b4c2c6a35443bc87bf2e1d746
Pierre LE GUEN <26087574+PierreLeGuen@users.noreply.github.com>
2023-06-02 16:18:12 +0200

    Delete beneficiary

---
+++ src/pages/beneficiary/manage.tsx
@@ -0,0 +1,72 @@
+import { XMarkIcon } from "@heroicons/react/20/solid";
+import { type Beneficiary } from "@prisma/client";
+import { getSidebarLayout } from "~/components/Layout";
+import { api } from "~/lib/api";
+import usePersistingStore from "~/store/useStore";
+import { type NextPageWithLayout } from "../_app";
+
+const Add: NextPageWithLayout = () => {
+  const { currentTeam } = usePersistingStore();
+  const mut = api.teams.deleteBeneficiaryForTeam.useMutation();
+
+  if (!currentTeam) {
+    throw new Error("No current team");
+  }
+
+  const {
+    data: benefs,
+    isLoading,
+    refetch,
+  } = api.teams.getBeneficiariesForTeam.useQuery({
+    teamId: currentTeam.id,
+  });
+
+  const deleteBeneficiary = (b: Beneficiary) => {
+    if (!currentTeam) {
+      throw new Error("No current team");
+    }
+    mut.mutate(
+      {
+        beneficiaryId: b.id,
+        teamId: currentTeam.id,
+      },
+      {
+        onSettled: () => {
+          void refetch();
+        },
+      }
+    );
+  };
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Manage Beneficiaries</h1>
+      {benefs?.map((b) => (
+        <div className="flex flex-row items-center gap-3" key={b.id}>
+          <div>
+            {b.firstName} {b.lastName}: {b.walletAddress}
+          </div>
+          <div>
+            <button
+              type="button"
+              onClick={() => {
+                deleteBeneficiary(b);
+              }}
+              className="inline-flex items-center justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+            >
+              <XMarkIcon className="h-4 w-4" />
+              <span>Remove</span>
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+Add.getLayout = getSidebarLayout;
+
+export default Add;
