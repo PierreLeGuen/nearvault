8c015a475a7bb4b897f8e702a80300860ad88ba9
Pierre LE GUEN <26087574+PierreLeGuen@users.noreply.github.com>
2023-06-02 18:20:54 +0200

    Fix beneficiary

--- src/components/Payments/BeneficiariesDropDown.tsx
+++ src/components/Payments/BeneficiariesDropDown.tsx
@@ -2,7 +2,6 @@ import { Combobox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 import { type Beneficiary } from "@prisma/client";
 import { Fragment, useState } from "react";
-import { type WalletPretty } from "~/pages/staking/stake";
 
 const BeneficiariesDropDown = ({
   beneficiaries,
@@ -35,9 +34,7 @@ const BeneficiariesDropDown = ({
         <div className="relative z-20 w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
           <Combobox.Input
             className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-            displayValue={(walletPretty: WalletPretty) =>
-              walletPretty.prettyName
-            }
+            displayValue={(b: Beneficiary) => b.firstName + " " + b.lastName}
             onChange={(event) => setQuery(event.target.value)}
           />
           <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
