7deded040a069d8625ce1cd1ad86bd868e7bde67
Volodymyr Bilyk <eclipseeer@gmail.com>
2024-01-05 11:56:57 +0200

    Fix wallets modal styles

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -30,88 +30,89 @@ export function Sidebar() {
   );
 
   return (
-    <div className="sticky top-0 flex h-screen w-[275px] flex-col justify-between gap-3 border-r-2">
-      <div className="flex flex-col">
-        <TeamsDropdownMenu />
-        <SidebarSection>
-          <SidebarSectionHeader>Treasury</SidebarSectionHeader>
-          <SidebarSectionBody>
-            <SidebarSectionItem href="/lockup/create">
-              <BanknotesIcon className={iconsClasses} />
-              Create lockup
-            </SidebarSectionItem>
-            <SidebarSectionItem href="/lockup/manage">
-              <EyeIcon className={iconsClasses} />
-              Manage lockup
-            </SidebarSectionItem>
-            <SidebarSectionItem href="/staking/stake">
-              <ArrowUpTrayIcon className={iconsClasses} />
-              Stake
-            </SidebarSectionItem>
-            <SidebarSectionItem href="/staking/unstake">
-              <ArrowUturnDownIcon className={iconsClasses} />
-              Unstake
-            </SidebarSectionItem>
-            <SidebarSectionItem href="/staking/withdraw">
-              <ArrowDownTrayIcon className={iconsClasses} />
-              Withdraw
-            </SidebarSectionItem>
-          </SidebarSectionBody>
-        </SidebarSection>
+    <>
+      <div className="sticky top-0 flex h-screen w-[275px] flex-col justify-between gap-3 border-r-2">
+        <div className="flex flex-col">
+          <TeamsDropdownMenu />
+          <SidebarSection>
+            <SidebarSectionHeader>Treasury</SidebarSectionHeader>
+            <SidebarSectionBody>
+              <SidebarSectionItem href="/lockup/create">
+                <BanknotesIcon className={iconsClasses} />
+                Create lockup
+              </SidebarSectionItem>
+              <SidebarSectionItem href="/lockup/manage">
+                <EyeIcon className={iconsClasses} />
+                Manage lockup
+              </SidebarSectionItem>
+              <SidebarSectionItem href="/staking/stake">
+                <ArrowUpTrayIcon className={iconsClasses} />
+                Stake
+              </SidebarSectionItem>
+              <SidebarSectionItem href="/staking/unstake">
+                <ArrowUturnDownIcon className={iconsClasses} />
+                Unstake
+              </SidebarSectionItem>
+              <SidebarSectionItem href="/staking/withdraw">
+                <ArrowDownTrayIcon className={iconsClasses} />
+                Withdraw
+              </SidebarSectionItem>
+            </SidebarSectionBody>
+          </SidebarSection>
 
-        <SidebarSection>
-          <SidebarSectionHeader>Payments</SidebarSectionHeader>
-          <SidebarSectionBody>
-            <SidebarSectionItem href="/payments/transfers">
-              <ArrowUpTrayIcon className={iconsClasses} />
-              Transfers
-            </SidebarSectionItem>
-            <SidebarSectionItem href="/payments/history">
-              <ArchiveBoxIcon className={iconsClasses} />
-              History
-            </SidebarSectionItem>
-            <SidebarSectionItem href="/beneficiary/manage">
-              <BookOpenIcon className={iconsClasses} />
-              Address Book
-            </SidebarSectionItem>
-          </SidebarSectionBody>
-        </SidebarSection>
+          <SidebarSection>
+            <SidebarSectionHeader>Payments</SidebarSectionHeader>
+            <SidebarSectionBody>
+              <SidebarSectionItem href="/payments/transfers">
+                <ArrowUpTrayIcon className={iconsClasses} />
+                Transfers
+              </SidebarSectionItem>
+              <SidebarSectionItem href="/payments/history">
+                <ArchiveBoxIcon className={iconsClasses} />
+                History
+              </SidebarSectionItem>
+              <SidebarSectionItem href="/beneficiary/manage">
+                <BookOpenIcon className={iconsClasses} />
+                Address Book
+              </SidebarSectionItem>
+            </SidebarSectionBody>
+          </SidebarSection>
 
-        <SidebarSection>
-          <SidebarSectionHeader>Approvals</SidebarSectionHeader>
-          <SidebarSectionBody>
-            <SidebarSectionItem href="/approval/pending">
-              <QueueListIcon className={iconsClasses} /> Pending requests
-            </SidebarSectionItem>
-            <SidebarSectionItem href="/approval/manage">
-              <AdjustmentsVerticalIcon className={iconsClasses} /> Manage
-              wallets
-            </SidebarSectionItem>
-            <SidebarSectionItem href="/approval/create">
-              <PlusCircleIcon className={iconsClasses} /> Create multisig
-              wallets
-            </SidebarSectionItem>
-          </SidebarSectionBody>
-        </SidebarSection>
+          <SidebarSection>
+            <SidebarSectionHeader>Approvals</SidebarSectionHeader>
+            <SidebarSectionBody>
+              <SidebarSectionItem href="/approval/pending">
+                <QueueListIcon className={iconsClasses} /> Pending requests
+              </SidebarSectionItem>
+              <SidebarSectionItem href="/approval/manage">
+                <AdjustmentsVerticalIcon className={iconsClasses} /> Manage
+                wallets
+              </SidebarSectionItem>
+              <SidebarSectionItem href="/approval/create">
+                <PlusCircleIcon className={iconsClasses} /> Create multisig
+                wallets
+              </SidebarSectionItem>
+            </SidebarSectionBody>
+          </SidebarSection>
 
-        <SidebarSection>
-          <SidebarSectionHeader>Accounting</SidebarSectionHeader>
-          <SidebarSectionBody>
-            <SidebarSectionItem href={"/accounting/tta"}>
-              <PieChartIcon className={iconsClasses} /> Reports
-            </SidebarSectionItem>
-          </SidebarSectionBody>
-        </SidebarSection>
+          <SidebarSection>
+            <SidebarSectionHeader>Accounting</SidebarSectionHeader>
+            <SidebarSectionBody>
+              <SidebarSectionItem href={"/accounting/tta"}>
+                <PieChartIcon className={iconsClasses} /> Reports
+              </SidebarSectionItem>
+            </SidebarSectionBody>
+          </SidebarSection>
+        </div>
+
+        {selectedAccount ? (
+          <SelectAccount openWalletModal={openWalletModal} />
+        ) : (
+          <Button onClick={openWalletModal}>Connect Account</Button>
+        )}
       </div>
-
-      {selectedAccount ? (
-        <SelectAccount openWalletModal={openWalletModal} />
-      ) : (
-        <Button onClick={openWalletModal}>Connect Account</Button>
-      )}
-
       <WalletModal />
-    </div>
+    </>
   );
 }
 
