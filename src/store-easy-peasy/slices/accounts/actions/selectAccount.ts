18086472f81c276ce422ef3d75d6dd0936660410
Pierre LE GUEN <26087574+PierreLeGuen@users.noreply.github.com>
2024-01-23 14:34:01 +0100

    No pending requests message

--- src/store-easy-peasy/slices/accounts/actions/selectAccount.ts
+++ src/store-easy-peasy/slices/accounts/actions/selectAccount.ts
@@ -3,5 +3,4 @@ import type { SelectAccount } from "~/store-easy-peasy/slices/accounts/types";
 
 export const selectAccount: SelectAccount = action((slice, accountId) => {
   slice.selected = slice.map[accountId];
-  slice.list.push("dad");
 });
