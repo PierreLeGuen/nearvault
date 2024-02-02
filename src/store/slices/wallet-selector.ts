86fe40e5a864b204c4d00a6d11603442f3721e48
Pierre LE GUEN <26087574+PierreLeGuen@users.noreply.github.com>
2024-02-02 16:47:19 +0100

    wip - new wallet selector

--- src/store/slices/wallet-selector.ts
+++ src/store/slices/wallet-selector.ts
@@ -15,17 +15,18 @@ import { PublicKey } from "near-api-js/lib/utils";
 import { transactions, utils } from "near-api-js";
 import { LedgerSigner } from "~/store-easy-peasy/slices/wallets/slices/ledger/helpers/LedgerSigner";
 import { LedgerClient } from "~/store-easy-peasy/slices/wallets/slices/ledger/helpers/LedgerClient";
-import { devtools } from "zustand/middleware";
+import { devtools, persist } from "zustand/middleware";
 import { getActions } from "~/store-easy-peasy/slices/wallets/thunks/signAndSendTransaction/getActions";
 import { JsonRpcProvider } from "near-api-js/lib/providers";
 import { type StateCreator } from "zustand";
+import { NextRouter, Router } from "next/router";
 
 type PublicKeyStr = string;
 type AccountId = string;
 type Source = "ledger" | "mynearwallet";
 
-type PkAndAccounts = Map<PublicKeyStr, AccountId[]>;
-type PkAndSources = Map<PublicKeyStr, Source>;
+type PkAndAccounts = Record<PublicKeyStr, AccountId[]>;
+type PkAndSources = Record<PublicKeyStr, Source>;
 
 interface State {
   accounts: PkAndAccounts;
@@ -45,6 +46,8 @@ interface Actions {
     actions: Action[],
   ) => Promise<Transaction>;
   connectWithLedger: (derivationPath?: string) => Promise<PublicKeyStr>;
+  connectWithMyNearWallet: () => void;
+  handleMnwRedirect: (router: NextRouter) => Promise<void>;
   signAndSendTransaction: ({
     senderId,
     receiverId,
@@ -59,23 +62,46 @@ interface Actions {
   setSelectedPublicKey: (pk: PublicKeyStr) => void;
 }
 
-export const walletTerminator: StateCreator<State & Actions> = (set, get) => ({
-  accounts: new Map(),
-  sources: new Map(),
+export const createWalletTerminator: StateCreator<State & Actions> = (
+  set,
+  get,
+) => ({
+  accounts: {},
+  sources: {},
   selectedPublicKey: "",
   addAccounts: (newAccounts: PkAndAccounts, newSources: PkAndSources) => {
-    const currentAccounts = get().accounts;
-    const mergedAccounts = new Map([...currentAccounts, ...newAccounts]);
+    const accounts = get().accounts;
+    console.log("addAccounts", {
+      currentAccounts: accounts,
+      newAccounts,
+    });
 
-    // filter out values that are undefined in array
-    const filteredAccounts = new Map(
-      [...mergedAccounts].map(([k, v]) => [k, v.filter(Boolean)]),
-    );
+    Object.keys(newAccounts).forEach((key) => {
+      accounts[key] = accounts[key] || [];
+      console.log("addAccounts", {
+        key,
+        currentAccounts: accounts[key],
+        newAccounts: newAccounts[key],
+      });
 
-    const currentSources = get().sources;
-    const mergedSources = new Map([...currentSources, ...newSources]);
+      //filter out null and  remove duplicate
+      accounts[key] = Array.from(
+        new Set([...accounts[key], ...newAccounts[key]]),
+      ).filter((a) => Boolean(a));
+    });
 
-    set({ accounts: filteredAccounts, sources: mergedSources });
+    console.log(accounts);
+
+    const sources = get().sources;
+
+    Object.keys(newSources).forEach((key) => {
+      if (!newSources[key]) {
+        return;
+      }
+      sources[key] = newSources[key];
+    });
+
+    set({ accounts, sources });
 
     console.log("addAccounts", {
       accounts: get().accounts,
@@ -108,14 +134,65 @@ export const walletTerminator: StateCreator<State & Actions> = (set, get) => ({
 
     console.log("connectWithLedger", { filteredAccounts });
 
-    get().addAccounts(
-      new Map([[pkStr, filteredAccounts]]),
-      new Map([[pkStr, "ledger"]]),
-    );
+    // Using plain objects instead of Map
+    const newAccounts = { [pkStr]: filteredAccounts };
+    const newSources: Record<PublicKeyStr, Source> = { [pkStr]: "ledger" };
+
+    get().addAccounts(newAccounts, newSources);
 
     get().setSelectedPublicKey(pkStr);
 
     return pkStr;
+  },
+  connectWithMyNearWallet: async () => {
+    const loginUrl = new URL(config.urls.myNearWallet + "/login");
+    loginUrl.searchParams.set(
+      "success_url",
+      `${window.location.origin}/wallet-redirects/my-near-wallet?connectStatus=Allowed&returnTo=${window.location.pathname}`,
+    );
+    loginUrl.searchParams.set(
+      "failure_url",
+      `${window.location.origin}/wallet-redirects/my-near-wallet?connectStatus=Rejected&returnTo=${window.location.pathname}`,
+    );
+    window.location.assign(loginUrl);
+  },
+  handleMnwRedirect: async (router: NextRouter) => {
+    const currentUrl = new URL(window.location.href);
+    const searchParams = currentUrl.searchParams;
+
+    const connectStatus = searchParams.get("connectStatus");
+    const returnTo = searchParams.get("returnTo");
+
+    if (connectStatus === "Allowed") {
+      const publicKey = searchParams.get("all_keys").split(",")[0];
+      if (!publicKey) {
+        throw new Error("No public key found in the URL");
+      }
+
+      const accountId =
+        searchParams.get("accountId") || searchParams.get("account_id");
+      if (!accountId) {
+        throw new Error("No account ID found in the URL");
+      }
+
+      const newAccounts = { [publicKey]: [accountId] };
+      const newSources: Record<PublicKeyStr, Source> = {
+        [publicKey]: "mynearwallet",
+      };
+
+      get().addAccounts(newAccounts, newSources);
+
+      get().setSelectedPublicKey(publicKey);
+
+      console.log("handleMnwRedirect", {
+        accounts: get().accounts,
+        sources: get().sources,
+      });
+
+      await router.replace(returnTo);
+      return;
+    }
+    throw new Error("not supported yet");
   },
   createTx: async (
     publicKey: PublicKeyStr,
@@ -151,7 +228,7 @@ export const walletTerminator: StateCreator<State & Actions> = (set, get) => ({
       params.action,
       params.actions,
     );
-    const source = get().sources.get(selectedPk);
+    const source = get().sources[selectedPk];
     if (source === "ledger") {
       const signedTx = await get().signWithLedger(tx);
       const provider = new JsonRpcProvider({ url: config.urls.rpc });
@@ -171,8 +248,21 @@ export const walletTerminator: StateCreator<State & Actions> = (set, get) => ({
     return signedTransaction;
   },
   signWithMnw: async (tx: Transaction) => {
-    console.error("signWithMnw not implemented");
+    const signUrl = new URL(config.urls.myNearWallet + "/sign");
+
+    const encodedTx = tx.encode();
+    signUrl.searchParams.set(
+      "transactions",
+      Buffer.from(encodedTx).toString("base64"),
+    );
+
+    const callbackUrl = `${window.location.origin}/wallet-redirects/my-near-wallet?returnTo=${window.location.pathname}`;
+    signUrl.searchParams.set("callbackUrl", callbackUrl);
+
+    window.location.assign(signUrl);
   },
 });
 
-export const useWalletTerminator = create<State & Actions>(walletTerminator);
+export const useWalletTerminator = create<State & Actions>()(
+  devtools(persist(createWalletTerminator, { name: "wallet-terminator" })),
+);
